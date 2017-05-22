0d56fab8a640a9ac3eaeefd7ea921600d06cf260
Anthony Raj <araj@onrule.com>
2017-05-21 19:30:29 -0700

    Added source code to manage role based access control with Fault tolerance patterns

---
+++ src/com/araj/microservice/rbac/LoginAction.java
@@ -0,0 +1,44 @@
+package com.araj.microservice.rbac;
+
+import com.opensymphony.xwork2.ActionSupport;
+ 
+public class LoginAction extends ActionSupport {
+ 
+    private String userName;
+    private String password;
+ 
+    public String execute() {
+        
+    	return SUCCESS;
+    }
+   
+    public String getUserName() {
+        return userName;
+    }
+ 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public void validate() {
+    	System.out.println("Validation invoked !! ");
+        if (getUserName().length() == 0) {
+            addFieldError("userName", "UserName.required");
+        } else if (!getUserName().equals("demo")) {
+            addFieldError("userName", "Invalid User");
+        }
+        if (getPassword().length() == 0) {
+            addFieldError("password", getText("password.required"));
+        }
+        System.out.println("Username="+getUserName()+" | Password="+getPassword());
+    }
+    
+}
---
+++ src/com/araj/microservice/rbac/RolesAction.java
@@ -0,0 +1,10 @@
+package com.araj.microservice.rbac;
+
+public class RolesAction {
+
+	public String execute() {
+		String success = "success";
+		return success;
+	}
+	
+}
